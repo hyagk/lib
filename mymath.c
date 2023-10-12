686034a3306fa4dfb03f489d12fc7898529ec80f
hyagk <145639863+hyagk@users.noreply.github.com>
2023-10-12 22:17:25 +0900

    Update mymath.c

--- mymath.c
+++ mymath.c
@@ -1,5 +1,13 @@
-#include <stdio.h>
-
 int add(int a, int b) {
-    return a + b;
+  return(a+b);
+}
+
+int sub(int a, int b) {
+  return(a-b);
+}
+int mul(int a, int b) {
+  return(a*b);
+}
+int div(int a, int b) {
+  return(a/b);
 }
